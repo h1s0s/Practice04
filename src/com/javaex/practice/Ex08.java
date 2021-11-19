284c7b4b652f120661161bc48f47c40d3225b53d
한상선 <hs94122@naver.com>
2021-11-19 18:08:29 +0900

    2021-11-19 18:08

--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -5,15 +5,19 @@ public class Ex08 {
 	public static void main(String[] args) {
 		//Ex08 배열을 사용하여 1~45까지의 숫자 중 임의의 6개의 숫자를 출력하세요-미니로또
 		//
-		//중복을 어떻게 처리할지 고민.. if문 안에 for을 넣어야 하는지 for안에 if를 넣을지 생각
-		int[] rottoArray = new int[6];
-		for(int i=0; i<rottoArray.length; i++) {
-			rottoArray[i] = (int)(Math.random()*45)+1;
-		}
-		for(int i=0; i<rottoArray.length; i++) {
-			System.out.print(rottoArray[i]+"\t");
-		}
+		//다시 짜보기. 단순하게 생각하기!!
+		int[] lottoArray = new int[6];
 		
-		
+		for(int i=0; i<lottoArray.length; i++) {
+			lottoArray[i] = (int)(Math.random()*45)+1;
+			for(int y=0; y<i; y++) {
+				if(lottoArray[i]==lottoArray[y]) {
+					i--;//다시 돌아가서 배열 다시
+				}
+			}
+		}
+		for(int i=0; i<lottoArray.length; i++) {//정리된 배열 출력
+			System.out.print(lottoArray[i]+"\t");
+		}
 	}
 }
