1f58344d4a47a88ad5498d7e8dca991eb30855c6
한상선 <hs94122@naver.com>
2021-11-19 16:36:01 +0900

    2021-11-19 16:35

--- src/com/javaex/practice/Ex07.java
+++ src/com/javaex/practice/Ex07.java
@@ -9,20 +9,19 @@ public class Ex07 {
 		//      500원동전, 100원동전, 50원동전, 1원동전 각 몇개로 변환되는지 작성 하세요.
 		Scanner sc = new Scanner(System.in);
 		
-		int[] wonArray = new int[10];
-		wonArray[0] = 50000;
-		wonArray[1] = 10000;
-		wonArray[2] = 5000;
-		wonArray[3] = 1000;
-		wonArray[4] = 500;
-		wonArray[5] = 100;
-		wonArray[6] = 50;
-		wonArray[7] = 10;
-		wonArray[8] = 5;
-		wonArray[9] = 1;
+		System.out.print("금액: ");
+		int num = sc.nextInt();
+		int[] wonArray = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1};
+		int[] wonEaArray = new int[10];
 		
-		
-		
+		for(int i=0; i<wonArray.length; i++) {//지폐 갯수를 계산해 변수에 넣어줄 반복문
+			if(num/wonArray[i]!=0) {//입력한 금액에 지정 지폐가 들어가 있나?
+				wonEaArray[i] = wonEaArray[i]+(num/wonArray[i]);
+				num = num-(wonArray[i]*(num/wonArray[i]));//입력한 금액에 지정 지폐가 얼마나 들어있는지 계산하고 차감함
+			} 
+		}for(int i2=0; i2<wonArray.length; i2++) {//계산된 지폐 갯수를 출력해줄 반복문 
+			System.out.println(wonArray[i2]+"원:"+wonEaArray[i2]+"개");
+		}
 		sc.close();
 	}
 
--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -5,7 +5,7 @@ public class Ex08 {
 	public static void main(String[] args) {
 		//Ex08 배열을 사용하여 1~45까지의 숫자 중 임의의 6개의 숫자를 출력하세요-미니로또
 		//
-		//
+		//중복을 어떻게 처리할지 고민.. if문 안에 for을 넣어야 하는지 for안에 if를 넣을지 생각
 		int[] rottoArray = new int[6];
 		for(int i=0; i<rottoArray.length; i++) {
 			rottoArray[i] = (int)(Math.random()*45)+1;
