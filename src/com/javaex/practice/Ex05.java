363526fa3aee19c355d7824c0358cf33515fdbd2
한상선 <hs94122@naver.com>
2021-11-22 10:22:14 +0900

    2021-11-22 10:22 //수정

--- src/com/javaex/practice/Ex05.java
+++ src/com/javaex/practice/Ex05.java
@@ -12,11 +12,14 @@ public class Ex05 {
 		double push = 0;
 		double pushArray[] = new double[5];//5칸의 배열을 생성해라
 		
-		for(int i=0; i<pushArray.length; i++) {
+		for(int i=0; i<pushArray.length; i++) {//입력을 받아 pushArray배열에 넣는 반복문
 			push = sc.nextDouble();//입력해라
-			sum = sum+push;//입력한 값을 sum에 차곡차곡 모음
+			pushArray[i] = push;
 		}
-		System.out.println("평균은 "+sum/5+" 입니다.");//합계의 평균을 내라
+		for(int i=0; i<pushArray.length; i++) {
+			sum = sum+pushArray[i];//입력한 값을 sum에 차곡차곡 모음
+		}
+		System.out.println("평균은 "+sum/pushArray.length+" 입니다.");//합계의 평균을 내라
 		sc.close();
 
 	}
--- src/com/javaex/practice/Ex06.java
+++ src/com/javaex/practice/Ex06.java
@@ -5,13 +5,18 @@ public class Ex06 {
 	public static void main(String[] args) {
 		// Ex06 주어진 문자열의 공백을 ','(콤마) 로 변경 후 출력하세요.
 		char c[] = {'T','h','i','s',' ','i','s',' ','a',' ','p','e','n','c','i','l'};
-		for(int i=0; i<c.length; i++) {
+		for(int i=0; i<c.length; i++) {//수정전 문장 배열 출력
 			System.out.print(c[i]);
-		}System.out.println("");
-		c[4] = ',';
-		c[7] = ',';
-		c[9] = ',';
-		for(int i=0; i<c.length; i++) {
+		}
+		System.out.println("");//공백
+		
+		for(int i=0; i<c.length; i++) {//if문으로 ' '가 있는지 검사하며, ','로 수정해주는 반복문
+			if(c[i]==' ') {
+				c[i]=',';
+			}
+		}
+		
+		for(int i=0; i<c.length; i++) {//수정후 문장 배열 출력
 			System.out.print(c[i]);
 		}
 	}
