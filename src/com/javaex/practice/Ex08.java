237bba2a3f45f74f252078316b576f8105b9e6c1
27_짝수_한상선 <91957568+h1s0s@users.noreply.github.com>
2021-12-07 15:40:10 +0900

    Update Ex08.java

--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -8,7 +8,7 @@ public class Ex08 {
 		int[] lottoArray = new int[6];
 		
 		for(int i=0; i<lottoArray.length; i++) {
-			lottoArray[i] = (int)(Math.random()*6)+1;
+			lottoArray[i] = (int)(Math.random()*45)+1;
 			for(int y=0; y<i; y++) {
 				if(lottoArray[i]==lottoArray[y]) {
 					i--;//다시 돌아가서 배열 다시
